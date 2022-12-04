66915c6bec668283e4ba34c45a32931dae976da4
Nelson Woo <nelsonwoo@gmail.com>
2022-12-04 16:33:32 +0800

    Week 1 Day 7.

--- README.md
+++ README.md
@@ -14,4 +14,20 @@ I wanted that HK$10 train ticket so bad ... so I built a robot too, not the kind
 
 The biggest challenge in this project is the physical property of the tapping.  The dabs of salt water will react with the metal tip that touches the screen.  The rust built up at the tip must be cleaned regularly for the tapping to be precise.
 
-The robot was finished in 3 days, and my rank moved to 6000 on day 1, and 750 on day 2, and finally 64 on day 3.  Good enough I suppose, unless another 400 bots surface to the top again.
+The robot was finished in 3 evenings. My rank moved to 6000 on day 1, and 750 on day 2, and finally 64 on day 3.  Good enough unless another 400 bots surface to the top again.
+
+## Day 7 of Week 1
+
+My rank dropped to 191 at breakfast on the last day of the first week into the game.  I was still in the safe zone, but my wife was in the danger at rank 488.  She might get pushed off the cliff in a few hours.  We had to act fast to to get to the higher ground.
+
+I might be experience in C++ but I am new to Python.  I had restructured my code from procedural to oo but the dtor concept wasn't just the same, so any Python reader might find my code amateur because I really didn't know how to write proper Python code.
+
+The first thing I tweaked was about he GO! signal.  The game had stages and pauses in between.  At the start of a stage, the screen would show a block of white text that said READY! and then GO!.  I had struggled with starting manually too late or too soon with an invalid screen frame.  The algo must skip that frame if the white text was in it.  That was number one.  Surprisely this zero delay in each start of stage had helped to rank from 488 to 415.
+
+The second thing tweaked was the timing of the arms tapping because the arms did tap well most of the time but some error would occur and killed the game.  Why!?  Slowing down the arm movement would have significant impact to the overall score.  The best solution in these situations was a tea break.
+
+I swapped to play the game with my phone and immediately it played a perfect game.  So I concluded it was the dab of salt water again.  Though the contact was made, the capacitative property had deteoriated over time.  That explained why the game played well on switching phones with a fresh dab of salt water.
+
+Enough talk.  Wife's game went from 488 to 415.  Mine went from 191 to 26.  Swapping back to her phone ... rank no. 6.  I was deliriously happy with this achievement.  That was a big Green Crab!!
+
+We called the HK$20 bill "green crab."
--- src/taptapmachine.py
+++ src/taptapmachine.py
@@ -7,12 +7,16 @@ import RPi.GPIO as GPIO
 import pigpio # Less servo jitter. Need pigpiod running.
 import time
 
-# Servos and Arm positions.
+# Servo colors.
 RED   = 24 # GPIO pin.
 WHITE = 22 # GPIO pin.
 BLUE  = 17 # GPIO pin.
 GREY  = 23 # GPIO pin but not connected.  We assume the 4th color meaning none of the above.
 gpins = [RED, WHITE, BLUE, GREY]
+
+# Webcam resolution.
+frame_height = 480
+frame_width = 640
 
 class Color:
     @classmethod
@@ -26,13 +30,10 @@ class Color:
         if c == GREY:
             return "GREY "
 
-frame_height = 480
-frame_width = 640
-
 class Pixel:
-    move = 1 # Number of pixel to move in the moveXX() functions.
+    move = 1 # Number of pixel to move targets.
     color_differential = 32 # Differential of RGB values to call a specific color.
-    color_threshold = 48 # Minimum RGB value to identify a color.
+    color_threshold = 0 # Minimum RGB value to identify a color.
     def __init__(self, y=0, x=0):
         self.y = y
         self.x = x
@@ -79,42 +80,37 @@ class Pixel:
         b, g, r = ( pixel )
         return (255-b, 255-g, 255-r)
 
-#target = [Pixel(280,402), Pixel(278,370), Pixel(278,335), Pixel(275,303), Pixel(273,270), Pixel(271,240), Pixel(268,205), Pixel(265,189)]
-target = [Pixel(273,408), Pixel(271,376), Pixel(271,341), Pixel(271,309), Pixel(271,276), Pixel(272,246), Pixel(273,220)]
-        
 class Servo:
+    pwm = pigpio.pi()
     def __init__(self, gpin, angle=45.0):
-        global pwm
         self.gpin = gpin
         self.angleUp = angle # degrees.
         self.angleDisplacement = 9 # degrees. #<--- TUNE THIS
-        self.tapDuration = 0.07 # seconds. #<--- TUNE THIS
-        self.postTapDuration = 0.15 # seconds #<--- TUNE THIS
-        pwm.set_mode(self.gpin, pigpio.OUTPUT)
-        pwm.set_PWM_frequency(self.gpin, 50)  # My servo operates at 50Hz.
-        pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(self.angleUp)) # Move arm to the ready position.
+        self.tapDuration = 0.05 # seconds. #<--- TUNE THIS
+        self.postTapDuration = 0.11 # seconds #<--- TUNE THIS
+        Servo.pwm.set_mode(self.gpin, pigpio.OUTPUT)
+        Servo.pwm.set_PWM_frequency(self.gpin, 50)  # My servo operates at 50Hz.
+        Servo.pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(self.angleUp)) # Move arm to the ready position.
     def __del__(self):
-        global pwm
-        pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(90.0)) # Raise arm all the way up for maintenance.
+        Servo.pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(90.0)) # Raise arm all the way up for maintenance.
         time.sleep(self.tapDuration)
-        pwm.set_PWM_dutycycle(self.gpin, 0)
-        pwm.set_PWM_frequency(self.gpin, 0)
+        Servo.pwm.set_PWM_dutycycle(self.gpin, 0)
+        Servo.pwm.set_PWM_frequency(self.gpin, 0)
     def __repr__(self):
-        return f'servo[{Color.text(self.gpin)}] = Servo({Color.text(self.gpin)}, {self.angleUp}) # tapDur={self.tapDuration}s,{self.postTapDuration}s'
+        return f'{Color.text(self.gpin)}: Servo({Color.text(self.gpin)}, {self.angleUp}),'
     def tap(self):
-        global pwm
-        pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(self.angleUp-self.angleDisplacement)) # Lower arm.
+        Servo.pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(self.angleUp-self.angleDisplacement)) # Lower arm.
         time.sleep(self.tapDuration) # Wait slightly for the arm to tap.
-        pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(self.angleUp)) # Raise arm.
+        Servo.pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(self.angleUp)) # Raise arm.
         time.sleep(self.postTapDuration) # Wait slightly for the arm to return.
-    def tuneAngle(self, change=1):        
+    def tuneAngle(self, change=1):
         self.angleUp += change
         if self.angleUp < 12.0:
             self.angleUp = 12.0
         elif self.angleUp > 178.0:
-            self.angleUp = 178.0     
-        pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(self.angleUp)) # Raise arm to new position
-    
+            self.angleUp = 178.0
+        Servo.pwm.set_servo_pulsewidth(self.gpin, Servo.translateDegree(self.angleUp)) # Raise arm to new position
+
     @classmethod
     def translateDegree(cls, deg):
         """ Pulse width 500 is 0 deg; 2500 is 180 deg. """
@@ -124,167 +120,180 @@ class Servo:
             deg = 180.0
         return deg*2000.0/180.0 + 500.0
 
-servo = {}
+class TapTapMachine:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    def __init__(self):
+        # Initialize Servos.
+        self.servo = {
+            RED  : Servo(RED  , 14.0),
+            WHITE: Servo(WHITE, 41.0),
+            BLUE : Servo(BLUE , 18.0),
+            GREY : Servo(GREY , 45.0),
+            }
+        # There are only 8 pixels in the frame. The last pixel tracks the GO! signal.
+        self.target = [Pixel(297,444), Pixel(297,409), Pixel(297,374), Pixel(297,340), Pixel(297,305), Pixel(297,272), Pixel(300,239), Pixel(297,213), Pixel(207,361), ]
+    def cleanup(self):
+        # Report the servo and target settings in case the next run wants to know.
+        for gpin in gpins:
+            print(repr(self.servo[gpin]))
+            del self.servo[gpin]
+        print('self.target = [', end = '')
+        for tgt in self.target:
+            print(repr(tgt), end=', ')
+        print(']')
+    def printButtons(self, buttons):
+        ti = time.time()
+        print(ti, end=' ')
+        for k in buttons:
+            print(Color.text(k), end=' ')
+        print('')
+        return ti
+    def run(self):
+        safetyOn = True
+        targetMovable = 0
+        servoInFocus = GREY
+        oldButtons = []
+        newButtons = []
+        while True:
+            ret, frame = cap.read() # Capture 1 frame from image input.
+            if not ret:
+                break
 
-def init():
-    # Initialize Servos.
-    global pwm
+            # Display the image for a little while and handle keyboard.
+            if safetyOn:
+                for tgt in self.target:
+                    tgt.drawCrossHair(frame)
+            cv2.imshow('live video', frame)
+            key = cv2.waitKey(1)
+
+            # Quit.
+            if (key==ord('q')):
+                print('Quit')
+                break
+
+            # Flip the safety on/off.
+            elif (key==ord('s')):
+                safetyOn = not safetyOn
+                if safetyOn:
+                    print('Safety ON')
+                else:
+                    print('Safety OFF')
+
+            # Cycle through targets.
+            elif (key==ord('n')):
+                targetMovable += 1
+                if targetMovable >= len(self.target):
+                    targetMovable = 0
+
+            # Move target.
+            elif (key==ord('i')):
+                for idx in range(targetMovable, len(self.target)):
+                    self.target[idx].moveUp()
+                print(repr(self.target[targetMovable]))
+            elif (key==ord('j')):
+                for idx in range(targetMovable, len(self.target)):
+                    self.target[idx].moveLeft()
+                print(repr(self.target[targetMovable]))
+            elif (key==ord('k')):
+                for idx in range(targetMovable, len(self.target)):
+                    self.target[idx].moveDown()
+                print(repr(self.target[targetMovable]))
+            elif (key==ord('l')):
+                for idx in range(targetMovable, len(self.target)):
+                    self.target[idx].moveRight()
+                print(repr(self.target[targetMovable]))
+
+            # Test arms.
+            elif (key==ord('1')):
+                servoInFocus = WHITE
+                self.servo[servoInFocus].tap()
+                print(repr(self.servo[servoInFocus]))
+            elif (key==ord('2')):
+                servoInFocus = RED
+                self.servo[servoInFocus].tap()
+                print(repr(self.servo[servoInFocus]))
+            elif (key==ord('3')):
+                servoInFocus = BLUE
+                self.servo[servoInFocus].tap()
+                print(repr(self.servo[servoInFocus]))
+
+            # Fine-tune arm height.
+            elif (key==ord('+') or key==ord('=')):
+                self.servo[servoInFocus].tuneAngle(1.0)
+                print(repr(self.servo[servoInFocus]))
+            elif (key==ord('-') or key==ord('_')):
+                self.servo[servoInFocus].tuneAngle(-1.0)
+                print(repr(self.servo[servoInFocus]))
+
+            # Run servo tests.
+            elif (key==ord('?')):
+                for k in [WHITE, RED, BLUE]:
+                    for i in range(0, len(self.target)):
+                        self.servo[k].tap()
+                for i in range(0, len(self.target)):
+                    self.servo[WHITE].tap()
+                    self.servo[RED].tap()
+                    self.servo[BLUE].tap()
+
+
+            # Load the color code from image into newButtons.
+            newButtons = []
+            for tgt in self.target:
+                k = tgt.getColor(frame)
+                newButtons.append(k)
+
+            # Discard and try again if GREY is detected.
+            if GREY in newButtons:
+                continue
+
+            if (key==ord('p')):
+                self.printButtons(newButtons)
+            
+            # Sometimes I started too soon before the GO! signal. This prevents using that frame.
+            kGo = self.target[-1].getColor(frame)
+            if kGo == WHITE:
+                continue
+
+            # Main dish here.
+            if not safetyOn:
+                if not oldButtons == newButtons:
+                    #ti = self.printButtons(newButtons) # Debug print.
+                    #j = 0
+                    #cv2.imwrite(f'{ti}.{j}.png', frame)
+                    # Safety Off ... Fire !!!!!!!!
+                    for k in newButtons[:8]:
+                        self.servo[k].tap()
+                        #j += 1
+                        #ret, frame = cap.read() # To see what is in between taps.
+                        #cv2.imwrite(f'{ti}.{j}.png', frame)
+                    oldButtons = newButtons
+                    time.sleep(0.21) # Wait before taking another image.
+        #endwhile
+    #enddef
+
+def main():
+    # TO-DO: How to control precisely the cleanup of cap??
+
+    # Fire up the webcam and set capture properties.
     global cap
-    global target
-    global servo
-    pwm = pigpio.pi()
-    servo[RED  ] = Servo(RED  , 12.0) # tapDur=0.08s,0.17s
-    servo[WHITE] = Servo(WHITE, 41.0) # tapDur=0.08s,0.17s
-    servo[BLUE ] = Servo(BLUE , 13.0) # tapDur=0.08s,0.17s
-    servo[GREY ] = Servo(GREY , 45.0) # tapDur=0.08s,0.17s
-    
-    cap = cv2.VideoCapture(0)  # The image is from the video device.
+    cap = cv2.VideoCapture(0)
     cap.set( cv2.CAP_PROP_FRAME_WIDTH, frame_width )
     cap.set( cv2.CAP_PROP_FRAME_HEIGHT, frame_height )
     #cap.set( cv2.CAP_PROP_FPS, 15 )
+
+    # Run the machine.
+    tpm = TapTapMachine()
+    tpm.run()
+    tpm.cleanup()
     
-def cleanup():
-    global cap
-    global pwm
-    global servo
-    # Clean up.
-    for gpin in gpins:
-        print(repr(servo[gpin]))
-        del servo[gpin]
-    for tgt in target:
-        print(repr(tgt), end=', ')
-    print('')
+    # Done with the webcam.
     cap.release()
     cv2.destroyAllWindows()
-    
-def printButtons(buttons):
-    ti = time.time()
-    print(ti, end=' ')
-    for k in buttons:
-        print(Color.text(k), end=' ')
-    print('')
-    return ti
-
-def main():
-    global servo
-    
-    safetyOn = True
-    targetMovable = 0
-    servoInFocus = GREY
-    oldButtons = []
-    newButtons = []
-    
-    init()
-    
-    while True:
-        ret, frame = cap.read() # Capture 1 frame from image input.
-        if not ret:
-            break
-
-        # Display the image for a little while and handle keyboard.
-        if safetyOn:
-            for tgt in target:
-                tgt.drawCrossHair(frame)
-        cv2.imshow('live video', frame)
-        key = cv2.waitKey(1) #<----- update
-
-        # Quit.
-        if (key==ord('q')):
-            print('Quit')
-            break
-        
-        # Flip the safety on/off.
-        elif (key==ord('s')):
-            safetyOn = not safetyOn
-            if safetyOn:
-                print('Safety ON')
-            else:
-                print('Safety OFF')
-            
-        
-        # Cycle through targets.
-        elif (key==ord('n')):
-            targetMovable += 1
-            if targetMovable >= len(target):
-                targetMovable = 0
-                
-        # Move target.
-        elif (key==ord('i')):
-            for idx in range(targetMovable, len(target)):
-                target[idx].moveUp()
-            print(repr(target[targetMovable]))
-        elif (key==ord('j')):
-            for idx in range(targetMovable, len(target)):
-                target[idx].moveLeft()
-            print(repr(target[targetMovable]))
-        elif (key==ord('k')):
-            for idx in range(targetMovable, len(target)):
-                target[idx].moveDown()
-            print(repr(target[targetMovable]))
-        elif (key==ord('l')):
-            for idx in range(targetMovable, len(target)):
-                target[idx].moveRight()
-            print(repr(target[targetMovable]))
-                
-        # Test arms.
-        elif (key==ord('1')):
-            servoInFocus = WHITE
-            servo[servoInFocus].tap()
-            print(repr(servo[servoInFocus]))
-        elif (key==ord('2')):
-            servoInFocus = RED
-            servo[servoInFocus].tap()
-            print(repr(servo[servoInFocus]))
-        elif (key==ord('3')):
-            servoInFocus = BLUE
-            servo[servoInFocus].tap()
-            print(repr(servo[servoInFocus]))
-            
-        # Fine-tune arm height.
-        elif (key==ord('+') or key==ord('=')):
-            servo[servoInFocus].tuneAngle(1.0)
-            print(repr(servo[servoInFocus]))
-        elif (key==ord('-') or key==ord('_')):
-            servo[servoInFocus].tuneAngle(-1.0)
-            print(repr(servo[servoInFocus]))
-            
-        # Run servo tests.
-        elif (key==ord('?')):
-            for k in [WHITE, RED, BLUE]:
-                for i in range(0, len(target)):
-                    servo[k].tap()
-            for i in range(0, len(target)):
-                servo[WHITE].tap()
-                servo[RED].tap()
-                servo[BLUE].tap()
-
-
-        # Load the color code into newButtons.
-        newButtons = []
-        for tgt in target:
-            k = tgt.getColor(frame)
-            newButtons.append(k)
-            
-        if (key==ord('p')):
-            printButtons(newButtons)
-        
-        # Discard and try again if GREY is detected.
-        if GREY in newButtons:
-            continue
-
-        # Main dish here.
-        if not safetyOn:
-            if not oldButtons == newButtons:
-                #printButtons(newButtons)
-                #cv2.imwrite(f'{ti}.jpg', frame)
-                # Safety Off ... Fire !!!!!!!!
-                for k in newButtons:
-                    servo[k].tap()
-                oldButtons = newButtons
-                time.sleep(0.20)
-    #endwhile
-
-    cleanup()
 
 if __name__ == "__main__":
     main()
+
